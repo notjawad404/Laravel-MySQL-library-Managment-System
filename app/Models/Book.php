c7002567445c9becaa381ad8400771a854a646ac
jawadali000123 <jawadali000123@gmail.com>
2024-10-18 11:12:32 +0000

    Add models and migrations

---
+++ app/Models/Book.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'description', 'image', 'author_id', 'category_id'];
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
