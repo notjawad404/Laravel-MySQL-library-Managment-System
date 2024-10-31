ee1b235d9f25d9b491ea00198c889e04056e73ba
jawadali000123 <jawadali000123@gmail.com>
2024-10-31 18:50:05 +0000

    Update Book migration

--- database/migrations/2024_10_31_183941_create_books_table.php
+++ database/migrations/2024_10_31_183941_create_books_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('book', function (Blueprint $table) {
+        Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->text('description');
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('book');
+        Schema::dropIfExists('books');
     }
 };
